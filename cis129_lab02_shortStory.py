1e25808f0b30cc697dd44aacd1e3cc1aab817e47
cbowman3 <159509936+cbowman3@users.noreply.github.com>
2024-02-08 23:21:39 -0700

    Update cis129_lab02_shortStory.py
    deleted "hello world" replaced with short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+# Author: Cecilia Bowman
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+(print)"In a magical forest, a curious bunny named Benny discovered a talking rainbow."
+(print)"The rainbow, named Rosie, loved to share colorful stories about the adventures of friendly clouds and happy raindrops."
+(print)"Benny and Rosie became fast friends, and every day, they spread joy and laughter throughout the enchanted woods."
